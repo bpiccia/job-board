d3077d7cd61cc326681e586c58ec52c4f32fb017
Bruno Picciafuoco <b.picciafuoco@nofraud.com>
2024-03-05 12:42:26 -0300

    feat: Adding company queries.

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,17 +1,23 @@
-import { getJobs } from './db/jobs.js'
+import { getJob, getJobs } from './db/jobs.js'
 import { getCompany } from './db/companies.js'
 
 export const resolvers = {
     Query: {
         jobs: () => {
             return getJobs()
+        },
+        job: (_root, {id}) => {
+            return getJob(id)
+        },
+        company: (_root, {id}) => {
+            return getCompany(id)
         }
     },
 
     Job: {
         date: (job) => toIsoDate(job.createdAt),
         company: (job) => getCompany(job.companyId)
-    }
+    },
 
 }
 
