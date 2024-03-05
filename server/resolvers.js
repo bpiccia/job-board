e08cf0e54346c6b6fbeed7f435025587542f9386
Bruno Picciafuoco <b.picciafuoco@nofraud.com>
2024-03-04 23:06:02 -0300

    feat: Adding relationship resolvers.

---
+++ server/resolvers.js
@@ -0,0 +1,21 @@
+import { getJobs } from './db/jobs.js'
+import { getCompany } from './db/companies.js'
+
+export const resolvers = {
+    Query: {
+        jobs: () => {
+            return getJobs()
+        }
+    },
+
+    Job: {
+        date: (job) => toIsoDate(job.createdAt),
+        company: (job) => getCompany(job.companyId)
+    }
+
+}
+
+
+function toIsoDate (value) {
+    return value.slice(0, 'yyyy-mm-dd'.length)
+}
